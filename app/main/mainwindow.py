2dc98577ef656ddf6f65a3f18eefaae4bd55a3da
TBlexcelsior <lequoctuan.fwork@gmail.com>
2022-08-14 21:25:22 +0700

    Add simple bar chart

--- app/main/mainwindow.py
+++ app/main/mainwindow.py
@@ -1,12 +1,12 @@
-from re import S
-from PySide2.QtCore import QTimer, Qt, SIGNAL
-from PySide2.QtWidgets import *
-from PySide2 import QtUiTools
 import sys
 import random
 import mysql.connector as mdb
+from PySide2.QtCore import QTimer, Qt, SIGNAL
+from PySide2.QtWidgets import *
+from PySide2 import QtUiTools, QtWidgets
 from PySide2extn.RoundProgressBar import roundProgressBar
-
+from PySide2.QtCharts import QtCharts
+from PySide2.QtGui import QPainter
 HOST = 'localhost'
 DATABASE = 'smart_bin'
 USER = 'tblexcel'
@@ -18,6 +18,7 @@ class MainWindow(object):
         self.app = QApplication(sys.argv)
         loader = QtUiTools.QUiLoader()
         self.window = loader.load('./app/component/MainGUI_t_2.ui', None)
+
         # Get main frame
         self.main_frame = self.window.findChild(QFrame, 'drop_shadow_frame')
         self.chart_frame = self.window.findChild(QFrame, 'Chart')
@@ -25,18 +26,24 @@ class MainWindow(object):
         # Get child object
         ## Exit button
         self.exit_btn = self.window.findChild(QPushButton, 'exit_btn')
+
         ## Percent text
         self.org_percent = self.window.findChild(QLabel, 'org_percent')
         self.inorg_percent = self.window.findChild(QLabel, 'inorg_percent')
         self.other_percent = self.window.findChild(QLabel, 'other_percent')
+        
         ## Percent progress bar
         self.prog_1 = self.window.findChild(QFrame, 'progress_bar_1')
         self.prog_2 = self.window.findChild(QFrame, 'progress_bar_2')
         self.prog_3 = self.window.findChild(QFrame, 'progress_bar_3')
+        
         ## Sheet
         self.sheet_1 = self.window.findChild(QFrame, 'type_1')
         self.sheet_2 = self.window.findChild(QFrame, 'type_2')
         self.sheet_3 = self.window.findChild(QFrame, 'type_3')
+
+        ## Chart Frame
+        self.chart_widget = self.window.findChild(QWidget, 'chart_widget')
 
         # Process Bar Design
         self.rpb_1 = roundProgressBar()
@@ -75,6 +82,38 @@ class MainWindow(object):
         self.rpb_3.rpb_setPathWidth(10)
         self.rpb_3.rpb_setPathColor((255, 255, 255))
 
+        # Bar Graph Setting
+        self.type_chart = QtCharts.QChart()
+        self.type_chart.setParent(self.chart_widget)
+        self.type_chart.setGeometry(0, 0, 980, 550)
+        self.hour_bar = QtCharts.QBarSet('Number of Wastes')
+        self.bar_value = [40, 12, 32, 40, 43 ,12, 35, 65, 12, 1]
+        self.hour_bar.append(self.bar_value)
+
+        self._bar_series = QtCharts.QBarSeries()
+        self._bar_series.append(self.hour_bar)
+
+
+        self.type_chart.addSeries(self._bar_series)
+        self.type_chart.setTitle("Garbage")
+        self.hours = ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
+        self._axis_x = QtCharts.QBarCategoryAxis()
+        self._axis_x.append(self.hours)
+        self.type_chart.setAxisX(self._axis_x, self._bar_series)
+        self._axis_x.setRange('8:00', '17:00')
+
+        self._axis_y = QtCharts.QValueAxis()
+        self.type_chart.setAxisY(self._axis_y, self._bar_series)
+        self._axis_y.setRange(0, max(self.bar_value))
+
+        self.type_chart.legend().setVisible(True)
+        self.type_chart.legend().setAlignment(Qt.AlignBottom)
+        self._type_chart_view = QtCharts.QChartView(self.type_chart)
+        self._type_chart_view.setRenderHint(QPainter.Antialiasing)
+        self.chart_layout = QtWidgets.QHBoxLayout(self.chart_widget)
+        self.chart_layout.setContentsMargins(0, 0, 0, 0)
+        self.chart_layout.addWidget(self._type_chart_view)
+
         # Setting text position
         self.other_percent.setAlignment(Qt.AlignCenter)
         self.inorg_percent.setAlignment(Qt.AlignCenter)
@@ -90,30 +129,36 @@ class MainWindow(object):
         self.sheet_1_btn.setParent(self.sheet_1)
         self.sheet_1_btn.setGeometry(0, 0, 250, 400)
         self.sheet_1_btn.setStyleSheet('background-color: rgba(255, 255, 255, 0);')
-        self.sheet_1_btn.connect(self.sheet_1_btn, SIGNAL('clicked()'), self.test1)
+        self.sheet_1_btn.connect(self.sheet_1_btn, SIGNAL('clicked()'), self.sheet1_clicked)
 
         self.sheet_2_btn = QPushButton()
         self.sheet_2_btn.setParent(self.sheet_2)
         self.sheet_2_btn.setGeometry(0, 0, 250, 400)
         self.sheet_2_btn.setStyleSheet('background-color: rgba(255, 255, 255, 0);')
-        self.sheet_2_btn.connect(self.sheet_2_btn, SIGNAL('clicked()'), self.test3)
+        self.sheet_2_btn.connect(self.sheet_2_btn, SIGNAL('clicked()'), self.sheet2_clicked)
 
         self.sheet_3_btn = QPushButton()
         self.sheet_3_btn.setParent(self.sheet_3)
         self.sheet_3_btn.setGeometry(0, 0, 250, 400)
         self.sheet_3_btn.setStyleSheet('background-color: rgba(255, 255, 255, 0);')
-        self.sheet_3_btn.connect(self.sheet_3_btn, SIGNAL('clicked()'), self.test3)
+        self.sheet_3_btn.connect(self.sheet_3_btn, SIGNAL('clicked()'), self.sheet3_clicked)
 
         self.exit_btn.connect(self.exit_btn, SIGNAL('clicked()'), self.exit)
 
-    def test1(self):
+    def sheet1_clicked(self):
         self.main_frame.setVisible(False)
         self.chart_frame.setVisible(True)
+    def sheet2_clicked(self):
+        self.main_frame.setVisible(False)
+        self.chart_frame.setVisible(True)
+    def sheet3_clicked(self):
+        self.main_frame.setVisible(False)
+        self.chart_frame.setVisible(True)
+
     def exit(self):
         self.main_frame.setVisible(True)
         self.chart_frame.setVisible(False)
-    def test3(self):
-        print('check3')
+
     def set_percent(self):
         try:
             conn = mdb.connect(host = HOST,
